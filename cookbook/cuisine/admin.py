d022dfb53dd1c1cdb440024c18113d4123db526c
papunjay kumar <kumarpapunjay@gmail.com>
2021-02-02 09:59:04 +0530

    [papunjay].register the modul

--- cookbook/cuisine/admin.py
+++ cookbook/cuisine/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import cookbook_dishes,dish_Comment
+# Register your models here.
+admin.site.register(cookbook_dishes)
+# admin.site.register(Comment)
+admin.site.register(dish_Comment)
