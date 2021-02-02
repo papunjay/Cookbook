8b222bb4df551fa2ac94822627682451bb380277
papunjay kumar <kumarpapunjay@gmail.com>
2021-02-02 09:56:53 +0530

    [papunjay].add the urls for the cuisine views

---
+++ cookbook/cuisine/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from django.conf.urls.static import static
+from .import views
+
+#app_name='cuisine'
+
+urlpatterns = [ 
+   
+   path('',views.home,name="home"), 
+   path('search',views.search,name="search"),
+   path('add_dishes',views.add_dishes,name="add_dishes"),
+   path('<int:id>',views.dish_details,name="dish_details"),
+   path('<int:id>',views.dish_like,name="dish_like"),
+   path('about',views.about,name="about"), 
+   path('<int:id>',views.add_favourite,name="add_favourite"), 
+]
